11980e6693a3342cdda50ae4d7db0ec1bf4f2395
Yasin S <sekmanyasin@hotmail.com>
2021-08-25 01:13:20 +0300

    SQL

---
+++ Odev1_1.Select.sql
@@ -0,0 +1,23 @@
+﻿--SQL stands for Structured Query Language
+--SQL lets you access and manipulate databases
+--SQL became a standard of the American National Standards Institute (ANSI) in 1986, and of the International Organization for Standardization (ISO) in 1987
+	--SQL, Yapılandırılmış Sorgu Dili anlamına gelir SQL, veritabanlarına erişmenizi ve bunları değiştirmenizi sağlar SQL, 1986'da Amerikan Ulusal Standartlar Enstitüsü'nün (ANSI) ve 1987'de Uluslararası Standardizasyon Örgütü'nün (ISO) standardı oldu.
+
+--RDBMS stands for Relational Database Management System.
+--RDBMS is the basis for SQL, and for all modern database systems such as MS SQL Server, IBM DB2, Oracle, MySQL, and Microsoft Access.
+--The data in RDBMS is stored in database objects called tables. A table is a collection of related data entries and it consists of columns and rows.
+	--RDBMS, İlişkisel Veritabanı Yönetim Sistemi anlamına gelir. RDBMS, SQL'in ve MS SQL Server, IBM DB2, Oracle, MySQL ve Microsoft Access gibi tüm modern veritabanı sistemlerinin temelidir. RDBMS'deki veriler, tablolar adı verilen veritabanı nesnelerinde depolanır. Tablo, ilgili veri girişlerinin bir koleksiyonudur ve sütunlardan ve satırlardan oluşur.
+
+--To build a web site that shows data from a database, you will need:
+--An RDBMS database program (i.e. MS Access, SQL Server, MySQL)
+--To use a server-side scripting language, like PHP or ASP
+--To use SQL to get the data you want
+--To use HTML / CSS to style the page
+	--Bir veritabanındaki verileri gösteren bir web sitesi oluşturmak için şunlara ihtiyacınız olacak: Bir RDBMS veritabanı programı (yani MS Access, SQL Server, MySQL) PHP veya ASP gibi bir sunucu tarafı betik dili kullanmak için İstediğiniz verileri almak için SQL kullanmak için Sayfayı biçimlendirmek için HTML / CSS kullanmak için
+
+--RDBMS stands for Relational Database Management System.
+--RDBMS is the basis for SQL, and for all modern database systems such as MS SQL Server, IBM DB2, Oracle, MySQL, and Microsoft Access.
+--The data in RDBMS is stored in database objects called tables. A table is a collection of related data entries and it consists of columns and rows.
+	--RDBMS, İlişkisel Veritabanı Yönetim Sistemi anlamına gelir. RDBMS, SQL'in ve MS SQL Server, IBM DB2, Oracle, MySQL ve Microsoft Access gibi tüm modern veritabanı sistemlerinin temelidir. RDBMS'deki veriler, tablolar adı verilen veritabanı nesnelerinde depolanır. Tablo, ilgili veri girişlerinin bir koleksiyonudur ve sütunlardan ve satırlardan oluşur.
+
+Select (count)City Si from Customers where City = 'Berlin';
